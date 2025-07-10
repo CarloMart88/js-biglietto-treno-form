0770a03f44c766d9f7cfe03ea91269ba7935969a
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-10 17:03:41 +0200

    feat: adding mathRandom

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,19 @@
 button.addEventListener(`click` , function(e){
   e.preventDefault();
 
+const MathRandom = () => {
+  return Math.floor(Math.random() * 10000)
+}
+
+
+
   const owner = document.getElementById(`owner`).value;
   const distance = document.getElementById(`distance`).value; 
   const discount = document.getElementById(`discount`).value;
+  const math = document.getElementById(`math`);
 
-    let ticket  ;
+  math.innerHTML += MathRandom()
+    
     let price = distance * 0.21 ;
     let discountBaby = ((price * 20)/100);
     let discountOld = ((price * 40)/100);
@@ -27,6 +35,7 @@ button.addEventListener(`click` , function(e){
       document.getElementById(`show-price`).innerText = (price - discountOld).toFixed(2);
     }
     
+
 
   document.getElementById(`show-owner`).innerText = owner ;
    

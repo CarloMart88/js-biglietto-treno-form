dc51e69c30033184e6868b60396bb1ce79f91cdd
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-10 14:32:58 +0200

    feat: Html skelton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,32 @@
-//definizione variabili 
+// //definizione variabili 
 
-const age = parseInt(prompt("Inserisci la tua età"));
-const percorso = parseInt(prompt("Inserisci il numero dei km del percorso  "));
+// const age = parseInt(prompt("Inserisci la tua età"));
+// const percorso = parseInt(prompt("Inserisci il numero dei km del percorso  "));
 
-let price = percorso * 0.21 ;
+// let price = percorso * 0.21 ;
 
-let discountBaby = ((price * 20)/100);
-let discountOld = ((price * 40)/100);
+// let discountBaby = ((price * 20)/100);
+// let discountOld = ((price * 40)/100);
 
-let ticket  ;
+// let ticket  ;
 
 
-if(age < 18 ) {
+// if(age < 18 ) {
   
- let ticket = price - discountBaby ;
+//  let ticket = price - discountBaby ;
 
- parseFloat(console.log(ticket))
+//  parseFloat(console.log(ticket))
 
-}else if(age > 65) { 
+// }else if(age > 65) { 
   
- let ticket = price - discountOld ;
+//  let ticket = price - discountOld ;
 
- parseFloat(console.log(ticket))
+//  parseFloat(console.log(ticket))
 
-}else {
+// }else {
  
- let ticket = price *1;
+//  let ticket = price *1;
 
- parseFloat(console.log(ticket))
-}
+//  parseFloat(console.log(ticket))
+// }
 
